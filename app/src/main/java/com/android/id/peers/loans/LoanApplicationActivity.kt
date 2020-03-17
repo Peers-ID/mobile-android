b74df7c2033c8c05bdf1c1dcf58156d56171efa4
reynald <reynald.alexander@gmail.com>
2020-03-17 08:08:51 +0800

    Adding dynamic layout on LoanApplicationActivity.kt

--- app/src/main/java/com/android/id/peers/loans/LoanApplicationActivity.kt
+++ app/src/main/java/com/android/id/peers/loans/LoanApplicationActivity.kt
@@ -2,10 +2,18 @@ package com.android.id.peers.loans
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.LinearLayout
 import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet.*
 import com.android.id.peers.R
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import kotlinx.android.synthetic.main.activity_loan_application.*
 
 class LoanApplicationActivity : AppCompatActivity() {
 
@@ -14,13 +22,123 @@ class LoanApplicationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_loan_application)
         title = "Loan Application"
 
-        var numberOfLoan = findViewById<SeekBar>(R.id.number_of_loan_seek_bar)
-        numberOfLoan.max = 10
-        numberOfLoan.incrementProgressBy(100)
-//        numberOfLoan.setOnSeekBarChangeListener()
+        val minLoanText = findViewById<TextView>(R.id.min_loan_text)
+        val maxLoanText = findViewById<TextView>(R.id.max_loan_text)
+        val numberOfLoanText = findViewById<TextInputEditText>(R.id.number_of_loan)
+        val numberOfLoan = findViewById<SeekBar>(R.id.number_of_loan_seek_bar)
+        var minLoan = 500000
+        var stepLoan = 100000
+        var maxLoan = 5000000
+        minLoanText.text = String.format("%d", minLoan)
+        maxLoanText.text = String.format("%d", maxLoan)
+        numberOfLoan.progress = 0
+        numberOfLoan.max = maxLoan
+        numberOfLoan.progress = maxLoan/2
+        numberOfLoan.incrementProgressBy(stepLoan)
+        Toast.makeText(this, String.format("%d", numberOfLoan.progress), Toast.LENGTH_SHORT).show()
+        numberOfLoanText.setText(String.format("%d", numberOfLoan.progress))
+        numberOfLoan.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                numberOfLoanText.setText(String.format("%d", i/stepLoan*stepLoan))
+                if (numberOfLoan.progress < minLoan) {
+                    numberOfLoan.progress = minLoan
+                    numberOfLoanText.setText(String.format("%d", minLoan))
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+//                Toast.makeText(applicationContext, "start tracking", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+//                Toast.makeText(applicationContext, "stop tracking", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val minTenorText = findViewById<TextView>(R.id.min_tenor_text)
+        val maxTenorText = findViewById<TextView>(R.id.max_tenor_text)
+        val tenorText = findViewById<TextInputEditText>(R.id.tenor)
+        val tenor = findViewById<SeekBar>(R.id.tenor_seek_bar)
+        var minTenor = 200000
+        var stepTenor = 200000
+        var maxTenor = 2000000
+        minTenorText.text = String.format("%d", minTenor)
+        maxTenorText.text = String.format("%d", maxTenor)
+        tenor.progress = 0
+        tenor.max = maxTenor
+        tenor.progress = maxTenor/2
+        tenor.incrementProgressBy(stepTenor)
+        Toast.makeText(this, String.format("%d", tenor.progress), Toast.LENGTH_SHORT).show()
+        tenorText.setText(String.format("%d", tenor.progress))
+        tenor.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                // Display the current progress of SeekBar
+                tenorText.setText(String.format("%d", i/stepTenor*stepTenor))
+                if (tenor.progress < minTenor) {
+                    tenor.progress = minTenor
+                    tenorText.setText(String.format("%d", minTenor))
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+//                Toast.makeText(applicationContext, "start tracking", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+//                Toast.makeText(applicationContext, "stop tracking", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        var fee = 100000
+        var serviceFee = findViewById<TextView>(R.id.service_fee)
+        serviceFee.text = String.format("%d", fee)
+
+        val linearLayoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        linearLayoutParams.marginStart = (resources. getDimension(R.dimen.activity_horizontal_margin)).toInt()
+        linearLayoutParams.marginEnd = (resources. getDimension(R.dimen.activity_horizontal_margin)).toInt()
+        linearLayoutParams.topMargin = (resources. getDimension(R.dimen.activity_vertical_margin)).toInt()
+        linearLayoutParams.bottomMargin = (resources. getDimension(R.dimen.margin_between)).toInt()
+        val feeContainer1 = ConstraintLayout(this)
+        feeContainer1.id = ConstraintLayout.generateViewId()
+        feeContainer1.layoutParams = linearLayoutParams
+        this.loan_container.addView(feeContainer1)
+        val constraintSet = ConstraintSet()
+        val constraintLayoutParams = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+        val otherFeeText = TextView(this)
+        otherFeeText.id = TextView.generateViewId()
+        Log.d("Loan Activitiy", "other Fee Text ID : " + String.format("%d", otherFeeText.id))
+        otherFeeText.text = "Other Fee"
+        otherFeeText.layoutParams = constraintLayoutParams
+
+        val otherFee = TextView(this)
+        otherFee.id = TextView.generateViewId()
+        Log.d("Loan Activitiy", "other Fee ID : " + String.format("%d", otherFee.id))
+        otherFee.text = "2000000"
+        otherFee.layoutParams = constraintLayoutParams
+
+        feeContainer1.addView(otherFeeText)
+        feeContainer1.addView(otherFee)
+        constraintSet.clone(feeContainer1)
+
+        constraintSet.connect(otherFeeText.id, TOP, PARENT_ID, TOP)
+        constraintSet.connect(otherFeeText.id, START, PARENT_ID, START)
+        constraintSet.connect(otherFeeText.id, BOTTOM, PARENT_ID, BOTTOM)
+
+        constraintSet.connect(otherFee.id, TOP, PARENT_ID, TOP)
+        constraintSet.connect(otherFee.id, BOTTOM, PARENT_ID, BOTTOM)
+        constraintSet.connect(otherFee.id, END, PARENT_ID, END)
+
+        constraintSet.applyTo(feeContainer1)
+
+
     }
-
-//    var seek = seekBarChangeListener(){
-
-//    }
 }
